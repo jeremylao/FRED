9be3ce6d0e10106014c3ba14037f0b4f6634bdd8
Jeremy Lao <jeremylao12@yahoo.co.uk>
2017-02-01 22:54:53 -0500

    fred_csv

---
+++ FRED_GET_CSV.py
@@ -0,0 +1,101 @@
+from xml.dom.minidom import parse
+from array import array
+from urllib.request import urlopen
+from xml.dom import minidom
+from io import StringIO
+from lxml import etree
+from dateutil import parser
+
+import datetime as dt
+import xml.etree.ElementTree as ET
+import json, requests
+import xmltodict
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import csv
+
+api_key = input("Input API Key: ")
+api_key = '&api_key='+api_key
+
+number_series = int(input("How many series to analyze: ") )
+
+series_name_url = {}
+series_name_string = {}
+json_map = {}
+
+for x in range (0, number_series):
+    
+    series = input("What series: ")
+    start_date = input("Start Date (mm/dd/yyyy): ")
+    end_date = input("End Date (mm/dd/yyyy - '12/31/9999' for latest): ")
+    start_date = dt.datetime.strptime(start_date,"%m/%d/%Y").strftime("%Y-%m-%d")
+    end_date = dt.datetime.strptime(end_date,"%m/%d/%Y").strftime("%Y-%m-%d")
+    FRED = 'https://api.stlouisfed.org/fred/series/observations?'
+    xmlurl = FRED+ 'series_id='+series+'&observation_start=' + start_date + '&observation_end=' + end_date + api_key +'&file_type=json'
+    series_name_url[series] = xmlurl
+    series_name_string[x] = series
+    json_map[series] = requests.get(xmlurl)
+
+for x in range (0, number_series):
+    temp = series_name_string[x]
+    
+    json_string = json_map[series_name_string[x]]
+
+    data = json.loads(json_string.text)
+
+    number = data["count"]
+    data_array = pd.Series(number)
+    date_list = [number]
+
+    for y in range (0,number):
+        if data["observations"][y]["value"] =='.':
+            date_list.append(data["observations"][y]["date"])
+            data_array[y] = np.nan 
+
+        else:
+            date_list.append(data["observations"][y]["date"])
+            data_array[y] = float(data["observations"][y]["value"])
+
+
+    dates = dt.datetime.date(dt.datetime.strptime(date_list[1], "%Y-%m-%d"))
+
+    datelist = pd.date_range(pd.datetime.today(), periods = number).tolist()
+
+    for z in range (0,number):
+        tempdate = dt.datetime.strptime(date_list[z+1], "%Y-%m-%d")
+        datelist[z] =dt.datetime.date(tempdate)
+
+    myfile = open(series_name_string[x]+'.csv', 'wt', newline='')
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow( ('date', series_name_string[x]))
+
+    for x in range (0,number):
+        wr.writerow( (datelist[x], data_array[x]) )
+
+    myfile.close()
+
+    fig = plt.figure()
+    #print(series_name_string[x+1])
+    #temp = series_name_string[x+1]
+    fig.suptitle(temp, fontsize=14, fontweight='bold')
+
+    plt.plot(datelist, data_array)
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+
+
+
+
+#code that i used to debug in creating this interface
+
+#rolling = pd.Series(number)
+#rolling = pd.rolling_mean(gdp_array,12)
+#print(data["observations"][1]["date"])
+#for x in range (0,number):
+#    print(gdp_array[x])
+#dates = plt.dates.date2num(date_array)
+#start_date = "1990-01-01"
+#end_date = "9999-12-31"
